cedc4362d9cbf0f4a43520f7a4eaefbd597f063a
SpokoSpartan <wojtekspoton@gmail.com>
2018-10-11 09:33:05 +0200

    starter

---
+++ backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/NokiaInnovativeProjectApplication.java
@@ -0,0 +1,12 @@
+package com.nokia.library.nokiainnovativeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NokiaInnovativeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NokiaInnovativeProjectApplication.class, args);
+	}
+}
