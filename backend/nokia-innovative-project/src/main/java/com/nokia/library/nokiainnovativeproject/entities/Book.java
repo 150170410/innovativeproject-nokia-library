92f1318c506ee5ba59d687e13d2c9105922154f5
tubidubidam <jacekzalewski95@gmail.com>
2018-10-20 14:43:54 +0200

    removed repo tests, fixed entity annotations

--- backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/entities/Book.java
+++ backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/entities/Book.java
@@ -1,17 +1,14 @@
 package com.nokia.library.nokiainnovativeproject.entities;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Data
-@Builder
+@Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -22,10 +19,13 @@ public class Book implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Setter
 	private String title;
 
+	@Setter
 	private String authorName;
 
+	@Setter
 	private String authorSurname;
 
 }
