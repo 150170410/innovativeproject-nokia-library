650493d3c543b104bbff9a8ec5c74848a3d3412a
alex <kostiukov3@gmail.com>
2018-10-22 09:10:29 +0200

    Update backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/controllers/BookControllerTest.java
    tests to bookController added

--- backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/controllers/BookControllerTest.java
+++ backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/controllers/BookControllerTest.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RequiredArgsConstructor
 public class BookControllerTest {
 
@@ -28,9 +28,9 @@ public class BookControllerTest {
 	@Test
 	public void testCreateBook() throws Exception {
 		Map<String, Object> requestBody = new HashMap<>();
-		requestBody.put("title", "sum titel");
-		requestBody.put("authorName", "test test");
-		requestBody.put("authorSurname", "sum nome");
+		requestBody.put("title", "test title1");
+		requestBody.put("authorName", "test name1");
+		requestBody.put("authorSurname", "test surname1");
 
 		HttpHeaders requestHeaders = new HttpHeaders();
 		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
@@ -41,8 +41,52 @@ public class BookControllerTest {
 		Map<String, Object> apiResponse = restTemplate.postForObject(url, httpEntity, Map.class, Collections.emptyMap());
 
 		Assert.assertNotNull(apiResponse);
+	}
 
+	@Test
+	public void testGetBookById() throws Exception {
+		Map<String, Object> requestBody = new HashMap<>();
+		requestBody.put("title", "test title2");
+		requestBody.put("authorName", "test name2");
+		requestBody.put("authorSurname", "test surname2");
 
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> httpEntity = new HttpEntity<>(OBJECT_MAPPER.writeValueAsString(requestBody), requestHeaders);
+
+		String urlCreate = "http://localhost:8080" + Mappings.API_VERSION + Mappings.LIBRARY + Mappings.BOOKS;
+		Map<String, Object> apiResponseCreate = restTemplate.postForObject(urlCreate, httpEntity, Map.class, Collections.emptyMap());
+
+		String urlGetById = "http://localhost:8080" + Mappings.API_VERSION + Mappings.LIBRARY + Mappings.BOOKS_ID;
+		Map<String, Object> apiResponseGetById = restTemplate.getForObject(urlGetById, Map.class, apiResponseCreate.get("id"), Collections.emptyMap());
+		apiResponseGetById.put("id", apiResponseCreate.get("id"));
+		Assert.assertTrue(apiResponseGetById.equals(apiResponseCreate));
+	}
+
+	@Test
+	public void testDeleteBook() throws Exception {
+		Map<String, Object> requestBody = new HashMap<>();
+		requestBody.put("title", "test title3");
+		requestBody.put("authorName", "test name3");
+		requestBody.put("authorSurname", "test surname3");
+
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> httpEntity = new HttpEntity<>(OBJECT_MAPPER.writeValueAsString(requestBody), requestHeaders);
+
+		String urlCreate = "http://localhost:8080" + Mappings.API_VERSION + Mappings.LIBRARY + Mappings.BOOKS;
+		Map<String, Object> apiResponseCreate = restTemplate.postForObject(urlCreate, httpEntity, Map.class, Collections.emptyMap());
+
+		String urlGetById = "http://localhost:8080" + Mappings.API_VERSION + Mappings.LIBRARY + Mappings.BOOKS_ID;
+		Map<String, Object> apiResponseGetById = restTemplate.getForObject(urlGetById, Map.class, apiResponseCreate.get("id"), Collections.emptyMap());
+		apiResponseGetById.put("id", apiResponseCreate.get("id"));
+
+		Assert.assertTrue(apiResponseGetById.equals(apiResponseCreate));
+
+		String urlDelete = "http://localhost:8080" + Mappings.API_VERSION + Mappings.LIBRARY + Mappings.BOOKS_ID;
+		restTemplate.delete(urlDelete, apiResponseCreate.get("id"));
 	}
 
 }
