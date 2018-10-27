89484766a917fc0f3a1e50b947f3c5775c24ca74
tubidubidam <jacekzalewski95@gmail.com>
2018-10-27 02:33:45 +0200

    using postgres in develop profile

--- backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/test/NokiaInnovativeProjectApplicationTests.java
+++ backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/test/NokiaInnovativeProjectApplicationTests.java
@@ -1,6 +1,7 @@
 package com.nokia.library.nokiainnovativeproject.test;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
