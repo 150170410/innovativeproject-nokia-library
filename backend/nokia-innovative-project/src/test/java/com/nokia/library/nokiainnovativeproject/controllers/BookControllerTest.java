aa034b0b47561d9b5655ad35446dd12bdb626d9f
alex <kostiukov3@gmail.com>
2018-10-29 13:38:17 +0100

    tests for BookController added

--- backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/controllers/BookControllerTest.java
+++ backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/controllers/BookControllerTest.java
@@ -1,93 +1,108 @@
 package com.nokia.library.nokiainnovativeproject.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nokia.library.nokiainnovativeproject.entities.Book;
+import com.nokia.library.nokiainnovativeproject.services.BookService;
 import com.nokia.library.nokiainnovativeproject.utils.Mappings;
-import lombok.RequiredArgsConstructor;
-import org.junit.Assert;
+
+import org.hamcrest.Matchers;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@RequiredArgsConstructor
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("autotests")
 public class BookControllerTest {
 
-	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-	private static final RestTemplate restTemplate = new RestTemplate();
+	private static Book book;
+	private static ObjectMapper mapper;
+	private MockMvc mockMvc;
 	private static final String BASE_URL = "http://localhost:8080" + Mappings.API_VERSION + Mappings.LIBRARY;
 
-	@Test
-	public void testCreateBook() throws Exception {
-		Map<String, Object> requestBody = new HashMap<>();
-		requestBody.put("title", "test title1");
-		requestBody.put("authorName", "test name1");
-		requestBody.put("authorSurname", "test surname1");
+	@Mock
+	private BookService service;
 
-		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+	@InjectMocks
+	private BookController controller;
 
-		HttpEntity<String> httpEntity = new HttpEntity<>(OBJECT_MAPPER.writeValueAsString(requestBody), requestHeaders);
+	@BeforeAll
+	public static void init(){
+		mapper = new ObjectMapper();
+		book = new Book();
+		book.setTitle("test title");
+		book.setAuthorName("test name");
+		book.setAuthorSurname("test surname");
+	}
 
-		String url = BASE_URL +  Mappings.BOOKS;
-		Map<String, Object> apiResponse = restTemplate.postForObject(url, httpEntity, Map.class, Collections.emptyMap());
-
-		Assert.assertNotNull(apiResponse);
+	@BeforeEach
+	public void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
 
 	@Test
-	public void testGetBookById() throws Exception {
-		Map<String, Object> requestBody = new HashMap<>();
-		requestBody.put("title", "test title2");
-		requestBody.put("authorName", "test name2");
-		requestBody.put("authorSurname", "test surname2");
-
-		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-		HttpEntity<String> httpEntity = new HttpEntity<>(OBJECT_MAPPER.writeValueAsString(requestBody), requestHeaders);
-
-		String urlCreate = BASE_URL +  Mappings.BOOKS;
-		Map<String, Object> apiResponseCreate = restTemplate.postForObject(urlCreate, httpEntity, Map.class, Collections.emptyMap());
-
-		String urlGetById = BASE_URL +  Mappings.BOOKS_ID;
-		Map<String, Object> apiResponseGetById = restTemplate.getForObject(urlGetById, Map.class, apiResponseCreate.get("id"), Collections.emptyMap());
-		apiResponseGetById.put("id", apiResponseCreate.get("id"));
-		Assert.assertTrue(apiResponseGetById.equals(apiResponseCreate));
+	public void getBooksTest() throws Exception {
+		List<Book> books = new ArrayList<>();
+		books.add(book);
+		when(service.getAllBooks()).thenReturn(books);
+		mockMvc.perform(get(BASE_URL + Mappings.BOOKS)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].title", Matchers.is("test title")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].authorName", Matchers.is("test name")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].authorSurname", Matchers.is("test surname")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.*", Matchers.hasSize(1)));
 	}
 
 	@Test
-	public void testDeleteBook() throws Exception {
-		Map<String, Object> requestBody = new HashMap<>();
-		requestBody.put("title", "test title3");
-		requestBody.put("authorName", "test name3");
-		requestBody.put("authorSurname", "test surname3");
-
-		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-		HttpEntity<String> httpEntity = new HttpEntity<>(OBJECT_MAPPER.writeValueAsString(requestBody), requestHeaders);
-
-		String urlCreate = BASE_URL + Mappings.BOOKS;
-		Map<String, Object> apiResponseCreate = restTemplate.postForObject(urlCreate, httpEntity, Map.class, Collections.emptyMap());
-
-		String urlGetById = BASE_URL +  Mappings.BOOKS_ID;
-		Map<String, Object> apiResponseGetById = restTemplate.getForObject(urlGetById, Map.class, apiResponseCreate.get("id"), Collections.emptyMap());
-		apiResponseGetById.put("id", apiResponseCreate.get("id"));
-
-		Assert.assertTrue(apiResponseGetById.equals(apiResponseCreate));
-
-		String urlDelete = BASE_URL +  Mappings.BOOKS_ID;
-		restTemplate.delete(urlDelete, apiResponseCreate.get("id"));
+	public void createBookTest() throws Exception {
+		String jsonRequest = mapper.writeValueAsString(book);
+		when(service.createBook(book)).thenReturn(book);
+		mockMvc.perform(post(BASE_URL + Mappings.BOOKS)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonRequest))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is("test title")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.authorName", Matchers.is("test name")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.authorSurname", Matchers.is("test surname")));
 	}
 
+	@Test
+	public void getBookByIdTest() throws Exception {
+		when(service.getBookById(1L)).thenReturn(book);
+		mockMvc.perform(get(BASE_URL + Mappings.BOOKS_ID, 1L)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is("test title")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.authorName", Matchers.is("test name")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.authorSurname", Matchers.is("test surname")));
+	}
+
+	@Test
+	public void deleteBookTest() throws Exception {
+		mockMvc.perform(delete(BASE_URL + Mappings.BOOKS_ID, 1L)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+		verify(service).deleteBook(1L);
+	}
 }
