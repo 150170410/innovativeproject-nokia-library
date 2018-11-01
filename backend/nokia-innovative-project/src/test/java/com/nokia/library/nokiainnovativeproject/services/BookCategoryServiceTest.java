6accf3170468679a04dc0455244a32681054f55c
tubidubidam <jacekzalewski95@gmail.com>
2018-11-01 17:44:51 +0100

    updateBookCategoryTest

--- backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/services/BookCategoryServiceTest.java
+++ backend/nokia-innovative-project/src/test/java/com/nokia/library/nokiainnovativeproject/services/BookCategoryServiceTest.java
@@ -6,7 +6,6 @@ import com.nokia.library.nokiainnovativeproject.controllers.BookCategoryControll
 import com.nokia.library.nokiainnovativeproject.entities.BookCategory;
 import com.nokia.library.nokiainnovativeproject.utils.Mappings;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -60,37 +59,52 @@ public class BookCategoryServiceTest {
 	}
 
 	@Test
-	public void getBooksTest() throws Exception {
+	public void getBookCategoriesTest() throws Exception {
 		List<BookCategory> bookCategories = new ArrayList<>();
 		bookCategories.add(bookCategory);
 		when(service.getAllBookCategories()).thenReturn(bookCategories);
 		mockMvc.perform(get(BASE_URL + Mappings.BK_CAT)
-				.contentType(MediaType.APPLICATION_JSON)).andDo(print())
+				.contentType(MediaType.APPLICATION_JSON))
+				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$[0].bookCategoryName", Matchers.is("test name")))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.*", Matchers.hasSize(1)));
 	}
 
 	@Test
-	public void createBookTest() throws Exception {
+	public void createBookCategoryTest() throws Exception {
 		String jsonRequest = mapper.writeValueAsString(bookCategoryDTO);
 		when(service.createBookCategory(bookCategoryDTO)).thenReturn(bookCategory);
 		mockMvc.perform(post(BASE_URL + Mappings.BK_CAT)
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(jsonRequest)).andDo(print())
+				.content(jsonRequest))
+				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.bookCategoryName", Matchers.is("test name")));
 	}
 
 	@Test
-	public void updateBookTest() throws Exception {
+	public void updateBookCategoryTest() throws Exception {
+		BookCategoryDTO updatedDTO = new BookCategoryDTO("updated name");
+		BookCategory updatedBookCategory = new BookCategory();
+		updatedBookCategory.setBookCategoryName("updated name");
 
-		Assertions.fail("not finished yet");
+		String jsonRequest = mapper.writeValueAsString(updatedDTO);
+
+		when(service.updateBookCategory(1L, updatedDTO)).thenReturn(updatedBookCategory);
+		mockMvc.perform(post(BASE_URL + Mappings.BK_CAT_ID, 1L)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonRequest))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1L)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.bookCategoryName", Matchers.is("updated name")));
+
 	}
 
 
 	@Test
-	public void getBookByIdTest() throws Exception {
+	public void getBookCategoryByIdTest() throws Exception {
 		when(service.getBookCategoryById(1L)).thenReturn(bookCategory);
 		mockMvc.perform(get(BASE_URL + Mappings.BK_CAT_ID, 1L)
 				.contentType(MediaType.APPLICATION_JSON))
@@ -99,7 +113,7 @@ public class BookCategoryServiceTest {
 	}
 
 	@Test
-	public void deleteBookTest() throws Exception {
+	public void deleteBookCategoryTest() throws Exception {
 		mockMvc.perform(delete(BASE_URL + Mappings.BK_CAT_ID, 1L)
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
