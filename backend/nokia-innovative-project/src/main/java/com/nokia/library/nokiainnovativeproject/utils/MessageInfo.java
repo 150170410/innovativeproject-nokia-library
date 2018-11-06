70ada0375e934d6bb72ca01e5ea5ccf4b99f15b6
tubidubidam <jacekzalewski95@gmail.com>
2018-11-06 13:17:11 +0100

    MessageInfo class, fixed development profile

---
+++ backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/utils/MessageInfo.java
@@ -0,0 +1,13 @@
+package com.nokia.library.nokiainnovativeproject.utils;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class MessageInfo {
+
+	private Boolean success;
+	private Object object;
+	private String message;
+
+}
