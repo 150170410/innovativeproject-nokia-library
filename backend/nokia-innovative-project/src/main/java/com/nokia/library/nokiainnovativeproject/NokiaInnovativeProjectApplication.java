1f114ffef76a7e9a0a8921e13301ecb52c3654f2
SpokoSpartan <wojtekspoton@gmail.com>
2018-11-13 05:06:10 +0100

    security deleted

--- backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/NokiaInnovativeProjectApplication.java
+++ backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/NokiaInnovativeProjectApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -14,7 +13,6 @@ import java.util.Collections;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableWebSecurity
 public class NokiaInnovativeProjectApplication {
 
 	public static void main(String[] args) {
