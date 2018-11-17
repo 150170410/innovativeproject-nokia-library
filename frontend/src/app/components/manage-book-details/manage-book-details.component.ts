a5ffa4740ca50a0fe3bf3fdaa8fb564b297de059
SpokoSpartan <wojtekspoton@gmail.com>
2018-11-18 00:49:57 +0100

    selection of the author and date + others table

--- frontend/src/app/components/manage-book-details/manage-book-details.component.ts
+++ frontend/src/app/components/manage-book-details/manage-book-details.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { RestService } from '../../services/rest/rest.service';
 import { BookCategoryDTO } from '../../models/DTOs/BookCategoryDTO';
 import { AuthorDTO } from '../../models/DTOs/Author';
 import { BookDetailsDTO } from '../../models/DTOs/BookDetailsDTO';
 import { BookCategory } from '../../models/entites/BookCategory';
 import { MessageInfo } from '../../models/entites/MessageInfo';
-
+import {Author} from '../../models/entites/Author';
+import {BookDetails} from '../../models/entites/BookDetails';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Observable} from 'rxjs';
+import {MatAutocomplete, MatAutocompleteSelectedEvent, MatChipInputEvent} from '@angular/material';
+import {map, startWith} from 'rxjs/operators';
+import {logger} from 'codelyzer/util/logger';
 
 @Component({
 	selector: 'app-manage-book-details',
@@ -19,12 +25,83 @@ export class ManageBookDetailsComponent implements OnInit {
 	authorParams: FormGroup;
 	bookDetailsParams: FormGroup;
 
-	categories: BookCategory[] = [];
-	displayedColumns: string[] = ['id', 'bookCategoryName'];
+	bookCategories: BookCategory[] = [];
+	displayedCategoryColumns: string[] = ['id', 'bookCategoryName'];
+	authors: Author[] = [];
+  displayedAuthorsColumn: string[] = ['id', 'authorName', 'authorSurname'];
+	booksDetails: BookDetails[] = [];
+  displayedBookDetailColumns: string[] = ['id', 'title', 'isbn'];
+
+  myCategories: BookCategory[] = [];
+
+  /*
+  Chip list for authors.
+  */
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  authorCtrl = new FormControl();
+  filteredAuthors: Observable<string[]>;
+  myAuthors: any[] = [];
+  @ViewChild('authorInput') authorInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+  /*
+  Chip list for authors.
+  */
+
 
 	constructor(private formBuilder: FormBuilder,
 				private http: RestService) {
+
+	  this.filteredAuthors = this.authorCtrl.valueChanges.pipe(
+      startWith(null),
+      map((author: string | null) => author ? this._filter(author) : this.authors.slice()));
 	}
+
+	/*
+	Chip list for authors
+  */
+  add(event: MatChipInputEvent): void {
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
+
+      if ((value || '').trim()) {
+        this.myAuthors.push( {
+          id: Math.random(),
+          authorName: value.trim()
+        });
+      }
+
+      if (input) {
+        input.value = '';
+      }
+
+      this.authorCtrl.setValue(null);
+    }
+  }
+  remove(author, index): void {
+    if (index >= 0) {
+      this.myAuthors.splice(index, 1);
+    }
+  }
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.myAuthors.push(event.option.value);
+    this.authorInput.nativeElement.value = '';
+    this.authorCtrl.setValue(null);
+  }
+  private _filter(value: any): any[] {
+    return this.authors.filter(author => author.authorName.toLowerCase().includes(value.toLowerCase()));
+  }
+  /*
+	Chip list for authors
+	 */
+
+  compareWithFn(item1, item2) {
+    return item1 && item2 ? item1.id === item2.id : item1 === item2;
+  }
+
 
 	ngOnInit() {
 		this.initCategoriesForm();
@@ -36,6 +113,7 @@ export class ManageBookDetailsComponent implements OnInit {
 		this.categoryParams = this.formBuilder.group({
 			categoryName: ['', Validators.required]
 		});
+    this.getCategories();
 	}
 
 	initAuthorForm() {
@@ -44,6 +122,7 @@ export class ManageBookDetailsComponent implements OnInit {
 			authorSurname: ['', Validators.required],
 			authorDescription: ['', Validators.required]
 		});
+    this.getAuthors();
 	}
 
 	initBookDetailsForm() {
@@ -55,20 +134,14 @@ export class ManageBookDetailsComponent implements OnInit {
 			tableOfContents: ['', Validators.required],
 			title: ['', Validators.required]
 		});
+    this.getBookDetails();
 	}
 
 	createCategory(params: any) {
 		const body = new BookCategoryDTO(params.value.categoryName);
 		this.http.save('bookCategory/create', body).subscribe(() => {
 			console.log('category created');
-			this.getCategories();
 		});
-
-	}
-
-	async getCategories() {
-		const response: MessageInfo = await this.http.getAll('bookCategory/getAll');
-		this.categories = response.object;
 	}
 
 	createAuthor(params: any) {
@@ -80,12 +153,27 @@ export class ManageBookDetailsComponent implements OnInit {
 	}
 
 	createBookDetails(params: any) {
-
 		const body = new BookDetailsDTO(params.value.coverPictureUrl, params.value.dateOfPublication,
 			params.value.description, params.value.isbn,
-			params.value.tableOfContents, params.value.title, [], [])
+			params.value.tableOfContents, params.value.title, this.myAuthors, this.myCategories)
 		this.http.save('bookDetails/create', body).subscribe(() => {
 			console.log('book details created');
+			console.log(this.myCategories);
 		});
 	}
+
+  async getCategories() {
+    const response: MessageInfo = await this.http.getAll('bookCategory/getAll');
+    this.bookCategories = response.object;
+  }
+
+  async getAuthors() {
+	  const response: MessageInfo = await this.http.getAll('author/getAll');
+	  this.authors = response.object;
+  }
+
+  async getBookDetails() {
+	  const response: MessageInfo = await this.http.getAll('bookDetails/getAll');
+	  this.booksDetails = response.object;
+  }
 }
