0a5ccf5982053c85f1543352f415cdb9fa3dbb80
SpokoSpartan <wojtekspoton@gmail.com>
2018-11-20 16:57:29 +0100

    validations for frontend

--- frontend/src/app/components/manage-book-details/manage-book-details.component.ts
+++ frontend/src/app/components/manage-book-details/manage-book-details.component.ts
@@ -24,12 +24,19 @@ export class ManageBookDetailsComponent implements OnInit {
 	authorParams: FormGroup;
 	bookDetailsParams: FormGroup;
 
+
 	bookCategories: BookCategory[] = [];
 	displayedCategoryColumns: string[] = ['id', 'bookCategoryName'];
 	authors: Author[] = [];
   displayedAuthorsColumn: string[] = ['id', 'authorName', 'authorSurname'];
 	booksDetails: BookDetails[] = [];
   displayedBookDetailColumns: string[] = ['id', 'title', 'isbn'];
+
+  // variables helpful for mistakes catching
+  categorySubmitted = false;
+  authorSubmitted = false;
+  bookDetailsSubmitted = false;
+
 
   // variables for categories form
   selectedCategories: BookCategory[] = [];
@@ -45,6 +52,7 @@ export class ManageBookDetailsComponent implements OnInit {
   myAuthors: any[] = [];
   @ViewChild('authorInput') authorInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
 
 	constructor(private formBuilder: FormBuilder, private http: RestService) {
 	  this.filteredAuthors = this.authorCtrl.valueChanges.pipe(
@@ -103,59 +111,88 @@ export class ManageBookDetailsComponent implements OnInit {
 
 	initCategoriesForm() {
 		this.categoryParams = this.formBuilder.group({
-			categoryName: ['', Validators.required]
+      categoryName: ['', [Validators.required, Validators.maxLength(50)]]
 		});
     this.getCategories();
 	}
 
 	initAuthorForm() {
 		this.authorParams = this.formBuilder.group({
-			authorName: ['', Validators.required],
-			authorSurname: ['', Validators.required],
-			authorDescription: ['', Validators.required]
+			authorName: ['', [Validators.required, Validators.maxLength(300)]],
+			authorSurname: ['', [Validators.required, Validators.maxLength(300)]],
+      authorDescription: ['', Validators.maxLength(10000)]
 		});
     this.getAuthors();
 	}
 
 	initBookDetailsForm() {
 		this.bookDetailsParams = this.formBuilder.group({
-			coverPictureUrl: ['', Validators.required],
-			dateOfPublication: ['', Validators.required],
-			description: ['', Validators.required],
-			isbn: ['', Validators.required],
-			tableOfContents: ['', Validators.required],
-			title: ['', Validators.required]
+			coverPictureUrl: ['', Validators.maxLength(1000)],
+			dateOfPublication: ['', [Validators.required]],
+			description: ['', Validators.maxLength(1000)],
+			isbn: ['', [Validators.required, Validators.maxLength(13), Validators.minLength(10)]],
+			tableOfContents: ['', Validators.maxLength(100)],
+			title: ['', [Validators.required, Validators.maxLength(100)]]
 		});
     this.getBookDetails();
 	}
 
 	createCategory(params: any) {
-		const body = new BookCategoryDTO(params.value.categoryName);
+
+	  this.categorySubmitted = true;
+
+    if (this.categoryParams.invalid) {
+      console.log('mistakes in parameters')
+      return;
+    }
+
+    const body = new BookCategoryDTO(params.value.categoryName);
 		this.http.save('bookCategory/create', body).subscribe(() => {
 			console.log('category created');
 		});
+
+		this.categorySubmitted = false;
 	}
 
 	createAuthor(params: any) {
-		const body = new AuthorDTO(params.value.authorName, params.value.authorSurname, params.value.authorDescription);
+
+	  this.authorSubmitted = true;
+
+	  if (this.authorParams.invalid) {
+	    console.log('mistakes in parameters')
+	    return;
+    }
+
+	  const body = new AuthorDTO(params.value.authorName, params.value.authorSurname, params.value.authorDescription);
 		console.log(body);
 		this.http.save('author/create', body).subscribe(() => {
 			console.log('author created');
 		});
+
+		this.authorSubmitted = false;
 	}
 
 	createBookDetails(params: any) {
+
+	  this.bookDetailsSubmitted = true;
+
+	  if (this.bookDetailsParams.invalid || this.myAuthors.length === 0 || this.selectedCategories.length === 0 ) {
+	    console.log('mistakes in parameters')
+      return;
+    }
+
 		const body = new BookDetailsDTO(params.value.coverPictureUrl, params.value.dateOfPublication,
 			params.value.description, params.value.isbn,
 			params.value.tableOfContents, params.value.title, this.myAuthors, this.selectedCategories)
 		this.http.save('bookDetails/create', body).subscribe(() => {
 			console.log('book details created');
-			console.log(this.selectedCategories);
 		});
+
+		this.bookDetailsSubmitted = false;
 	}
 
   async getCategories() {
-    const response: MessageInfo = await this.http.getAll('bookCategory/getAll');
+	  const response: MessageInfo = await this.http.getAll('bookCategory/getAll');
     this.bookCategories = response.object;
   }
 
