71c45da18b3162b7a99f3d6b4e4bdcc60c751ab9
tubidubidam <jacekzalewski95@gmail.com>
2018-11-26 22:42:10 +0100

    email service frontend part

--- frontend/src/app/components/contact-us/contact-us.component.ts
+++ frontend/src/app/components/contact-us/contact-us.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EmailDTO } from '../../models/EmailDTO';
 import { MatDialogRef } from '@angular/material';
+import { RestService } from '../../services/rest/rest.service';
 
 @Component({
 	selector: 'app-contact-us',
@@ -13,10 +14,11 @@ export class ContactUsComponent implements OnInit {
 	contactParams: FormGroup;
 
 	categories = ['Report a bug', 'Request new feature', 'I don\'t like...', 'Other'];
-	recipients = ['nokia.library@gmail.com']; // TODO: to which emails these messages should be sent?
+	recipients = ['aabc0041@gmail.com']; // TODO: to which emails these messages should be sent?
 
 	constructor(private formBuilder: FormBuilder,
-				public dialogRef: MatDialogRef<ContactUsComponent>) {
+				public dialogRef: MatDialogRef<ContactUsComponent>,
+				private http: RestService) {
 	}
 
 	ngOnInit() {
@@ -35,13 +37,13 @@ export class ContactUsComponent implements OnInit {
 		const subject = contactParams.value.subject;
 		const category = contactParams.value.category;
 		const context = contactParams.value.message;
-		console.log(contactParams);
- 		const email = new EmailDTO(category + ' ' + subject, context, this.recipients);
+ 		const email = new EmailDTO(category + ': ' + subject, context, this.recipients);
  		console.log(email);
 
-		this.emailSent();
-
-		// TODO: send emails
+		this.http.save('email/create', email).subscribe(() => {
+			console.log('email sent');
+			this.emailSent();
+		});
 	}
 
 	emailSent(): void {
