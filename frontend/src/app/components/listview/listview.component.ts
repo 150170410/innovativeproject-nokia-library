7d07a4cc024d39cafb0688675c06e4c8b3841fe6
tubidubidam <jacekzalewski95@gmail.com>
2018-12-17 01:09:12 +0100

    listview item upgrade

--- frontend/src/app/components/listview/listview.component.ts
+++ frontend/src/app/components/listview/listview.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookDetailsService } from '../../services/book-details/book-details.service';
 import { BookDetails } from '../../models/database/entites/BookDetails';
 import { RestService } from '../../services/rest/rest.service';
@@ -7,12 +7,10 @@ import { MessageInfo } from '../../models/MessageInfo';
 @Component({
 	selector: 'app-listview',
 	templateUrl: './listview.component.html',
-	styleUrls: ['./listview.component.css'],
-	changeDetection: ChangeDetectionStrategy.OnPush,
+	styleUrls: ['./listview.component.css']
 })
-export class ListviewComponent implements OnInit, AfterViewInit {
+export class ListviewComponent implements OnInit {
 
-	items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
 	books: any;
 	allBooks: BookDetails[] = [];
 	errorMessage: any;
@@ -21,21 +19,10 @@ export class ListviewComponent implements OnInit, AfterViewInit {
 	}
 
 	ngOnInit() {
-
+		this.getBooksDetails();
 	}
 
-	ngAfterViewInit(): void {
-		this.getBooks();
-	}
-
-
-	async getBooks(id?: number) {
-		this.books = await this.bookDetailsService.getBooks(id)
-		.catch((err) => {
-			console.log(err.message);
-			this.errorMessage = err;
-		});
-
+	async getBooksDetails() {
 		const response: MessageInfo = await this.http.getAll('bookDetails/getAll');
 		this.allBooks = response.object.sort().reverse();
 	}
