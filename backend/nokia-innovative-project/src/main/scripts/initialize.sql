1138879819f1c9e1a97ca1c06d5bee9f7e1ba486
tubidubidam <jacekzalewski95@gmail.com>
2018-12-19 06:22:54 +0100

    init script with actually existing books

--- backend/nokia-innovative-project/src/main/scripts/initialize.sql
+++ backend/nokia-innovative-project/src/main/scripts/initialize.sql
@@ -3,98 +3,114 @@ INSERT INTO book_status (id, status_name) VALUES (2, 'BORROWED');
 INSERT INTO book_status (id, status_name) VALUES (3, 'RESERVED');
 INSERT INTO book_status (id, status_name) VALUES (4, 'UNKNOWN');
 
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://about.canva.com/wp-content/uploads/sites/3/2015/01/art_bookcover.png'
-    , '2018-02-02'
-    , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit elit ut venenatis feugiat. Suspendisse maximus dui ac risus ultricies, a accumsan risus semper. Etiam eu fringilla amet. '
-    , '56756756784'
-    , 'The World of Abstract Art');
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://www.creativindie.com/wp-content/uploads/2013/10/Enchantment-Book-Cover-Best-Seller1.jpg'
-    , '1999-01-02'
-    , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit elit ut venenatis feugiat. Suspendisse maximus dui ac risus ultricies, a accumsan risus semper. Etiam eu fringilla amet. '
-    , '12312312314'
-    , 'Enchantment');
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://cdn-images-1.medium.com/max/1200/1*cNAPNAIDIHH0G-toMJhjxg.png'
-    , '1996-01-05'
-    , 'There''s nothing that hard-core Unix and Linux users are more fanatical about than their text editor. Editors are the subject of adoration and worship, or of scorn and ridicule, depending upon whether the topic of discussion is your editor or someone else''s.
-vi has been the standard editor for close to 30 years. Popular on Unix and Linux, it has a growing following on Windows systems, too. Most experienced system administrators cite vi as their tool of choice. And since 1986, this book has been the guide for vi. '
-    , '456347569012'
-    , 'Learning The vi');
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://i.pinimg.com/236x/ee/15/e2/ee15e271f8e2467560ecf0d12ffb8ca5--o-reilly-programming.jpg'
-    , '1980-01-05'
-    , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit elit ut venenatis feugiat. Suspendisse maximus dui ac risus ultricies, a accumsan risus semper. Etiam eu fringilla amet. '
-    , '23423423411'
-    , 'Banishing The Microsoft');
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://i.pinimg.com/474x/f2/39/34/f239346d5830f9382eee8400e990959c--software-development-computer-science.jpg'
-    , '1980-01-05'
-    , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit elit ut venenatis feugiat. Suspendisse maximus dui ac risus ultricies, a accumsan risus semper. Etiam eu fringilla amet. '
-    , '76576565444'
-    , 'Useless Git Commit Messages');
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://i.pinimg.com/564x/70/bd/5f/70bd5f632826d5d0c6ee035e6ce97ddf.jpg'
-    , '2010-02-02'
-    , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit elit ut venenatis feugiat. Suspendisse maximus dui ac risus ultricies, a accumsan risus semper. Etiam eu fringilla amet. '
-    , '76576576543'
-    , 'Solving Imaginary Scaling Issues');
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://d3ansictanv2wj.cloudfront.net/expect-2-d45f737c6b69f2c3ff53c765456ca541.jpg'
-    , '1980-01-05'
-    , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit elit ut venenatis feugiat. Suspendisse maximus dui ac risus ultricies, a accumsan risus semper. Etiam eu fringilla amet. '
-    , '76576576576'
-    , 'Exploring Expect');
-INSERT INTO book_details (cover_picture_url, publication_date, description, isbn, title) VALUES
-  ('https://bit.ly/2DJuhzi'
-    , '1980-01-05'
-    , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit elit ut venenatis feugiat. Suspendisse maximus dui ac risus ultricies, a accumsan risus semper. Etiam eu fringilla amet. '
-    , '23523523525'
-    , 'The Lives Inside Your Head');
+INSERT INTO book_category (id, book_category_name) VALUES (1, 'Guide');
+INSERT INTO book_category (id, book_category_name) VALUES (2, 'Programming');
+INSERT INTO book_category (id, book_category_name) VALUES (3, 'Novel');
+INSERT INTO book_category (id, book_category_name) VALUES (4, 'Cooking');
+INSERT INTO book_category (id, book_category_name) VALUES (5, 'Autobiography');
+INSERT INTO book_category (id, book_category_name) VALUES (6, 'Politics');
+INSERT INTO book_category (id, book_category_name) VALUES (7, 'Sci-fi');
+INSERT INTO book_category (id, book_category_name) VALUES (8, 'Religion');
+INSERT INTO book_category (id, book_category_name) VALUES (9, 'Education');
+INSERT INTO book_category (id, book_category_name) VALUES (10, 'Astronomy');
+INSERT INTO book_category (id, book_category_name) VALUES (11, 'Science');
+INSERT INTO book_category (id, book_category_name) VALUES (12, 'Java');
 
-INSERT INTO book_category (book_category_name) VALUES ('Guide');
-INSERT INTO book_category (book_category_name) VALUES ('Programming');
-INSERT INTO book_category (book_category_name) VALUES ('Novel');
-INSERT INTO book_category (book_category_name) VALUES ('Cooking');
-INSERT INTO book_category (book_category_name) VALUES ('Autobiography');
-INSERT INTO book_category (book_category_name) VALUES ('Politics');
-INSERT INTO book_category (book_category_name) VALUES ('Sci-fi');
-INSERT INTO book_category (book_category_name) VALUES ('Religion');
-INSERT INTO book_category (book_category_name) VALUES ('Education');
+INSERT INTO author (id, author_full_name) VALUES (1, 'Edgar Jander');
+INSERT INTO author (id, author_full_name) VALUES (2, 'Lucas Newell');
+INSERT INTO author (id, author_full_name) VALUES (3, 'Robbie Torok');
+INSERT INTO author (id, author_full_name) VALUES (4, 'Raymond Kertis');
+INSERT INTO author (id, author_full_name) VALUES (5, 'Emmet Brenig');
+INSERT INTO author (id, author_full_name) VALUES (6, 'Andrzej Sapkowski');
+INSERT INTO author (id, author_full_name) VALUES (7, 'Sejal Badani');
+INSERT INTO author (id, author_full_name) VALUES (8, 'Rhys Bowen');
+INSERT INTO author (id, author_full_name) VALUES (9, 'Melinda Leigh');
+INSERT INTO author (id, author_full_name) VALUES (10, 'Jeff Kinney');
+INSERT INTO author (id, author_full_name) VALUES (11, 'Michelle Obama');
+INSERT INTO author (id, author_full_name) VALUES (12, 'Samin Nosrat');
+INSERT INTO author (id, author_full_name) VALUES (13, 'Stephen Hawking');
+INSERT INTO author (id, author_full_name) VALUES (14, 'George Orwell');
 
-INSERT INTO book_details_categories VALUES (1, 1);
+
+INSERT INTO book_details (id, isbn, title, description, cover_picture_url, publication_date) VALUES (
+  1,
+  '9781542048279',
+  'The Storytellers Secret: A Novel',
+  'Nothing prepares Jaya, a New York journalist, for the heartbreak of her third miscarriage and the slow unraveling of her marriage in its wake. Desperate to assuage her deep anguish, she decides to go to India to uncover answers to her family’s past.Intoxicated by the sights, smells, and sounds she experiences, Jaya becomes an eager student of the culture. But it is Ravi—her grandmother’s former servant and trusted confidant—who reveals the resilience, struggles, secret love, and tragic fall of Jaya’s pioneering grandmother during the British occupation. Through her courageous grandmother’s arrestingly romantic and heart-wrenching story, Jaya discovers the legacy bequeathed to her and a strength that, until now, she never knew was possible.',
+  'https://images-na.ssl-images-amazon.com/images/I/51GC3g1SSXL._SX331_BO1,204,203,200_.jpg',
+  '2018-09-01');
+INSERT INTO book_details (id, isbn, title, description, cover_picture_url, publication_date) VALUES (
+  2,
+  '9781503941359',
+  'In Farleigh Field: A Novel of World War II',
+  'World War II comes to Farleigh Place, the ancestral home of Lord Westerham and his five daughters, when a soldier with a failed parachute falls to his death on the estate. After his uniform and possessions raise suspicions, MI5 operative and family friend Ben Cresswell is covertly tasked with determining if the man is a German spy. The assignment also offers Ben the chance to be near Lord Westerham’s middle daughter, Pamela, whom he furtively loves. But Pamela has her own secret: she has taken a job at Bletchley Park, the British code-breaking facility.As Ben follows a trail of spies and traitors, which may include another member of Pamela’s family, he discovers that some within the realm have an appalling, history-altering agenda. Can he, with Pamela’s help, stop them before England falls?Inspired by the events and people of World War II, writer Rhys Bowen crafts a sweeping and riveting saga of class, family, love, and betrayal.',
+  'https://images-na.ssl-images-amazon.com/images/I/51Xg26g1znL._SX331_BO1,204,203,200_.jpg',
+  '2017-03-01');
+INSERT INTO book_details (id, isbn, title, description, cover_picture_url, publication_date) VALUES (
+  3,
+  '9781419727436',
+  'Diary of a Wimpy Kid #13: Meltdown',
+  'When snow shuts down Greg Heffley’s middle school, his neighborhood transforms into a wintry battlefield. Rival groups fight over territory, build massive snow forts, and stage epic snowball fights. And in the crosshairs are Greg and his trusty best friend, Rowley Jefferson.It’s a fight for survival as Greg and Rowley navigate alliances, betrayals, and warring gangs in a neighborhood meltdown. When the snow clears, will Greg and Rowley emerge as heroes? Or will they even survive to see another day?',
+  'https://images-na.ssl-images-amazon.com/images/I/512EFepj8GL._SX340_BO1,204,203,200_.jpg',
+  '2018-10-30');
+INSERT INTO book_details (id, isbn, title, description, cover_picture_url, publication_date) VALUES (
+  4,
+  '9781524763138',
+  'Becoming',
+  'In a life filled with meaning and accomplishment, Michelle Obama has emerged as one of the most iconic and compelling women of our era. As First Lady of the United States of America—the first African American to serve in that role—she helped create the most welcoming and inclusive White House in history, while also establishing herself as a powerful advocate for women and girls in the U.S. and around the world, dramatically changing the ways that families pursue healthier and more active lives, and standing with her husband as he led America through some of its most harrowing moments. Along the way, she showed us a few dance moves, crushed Carpool Karaoke, and raised two down-to-earth daughters under an unforgiving media glare.In her memoir, a work of deep reflection and mesmerizing storytelling, Michelle Obama invites readers into her world, chronicling the experiences that have shaped her—from her childhood on the South Side of Chicago to her years as an executive balancing the demands of motherhood and work, to her time spent at the world’s most famous address. With unerring honesty and lively wit, she describes her triumphs and her disappointments, both public and private, telling her full story as she has lived it—in her own words and on her own terms. Warm, wise, and revelatory, Becoming is the deeply personal reckoning of a woman of soul and substance who has steadily defied expectations—and whose story inspires us to do the same.',
+  'https://images-na.ssl-images-amazon.com/images/I/414JfiBCutL._SX327_BO1,204,203,200_.jpg',
+  '2018-11-13');
+INSERT INTO book_details (id, isbn, title, description, cover_picture_url, publication_date) VALUES (
+  5,
+  '9781476753836',
+  'Salt, Fat, Acid, Heat: Mastering the Elements of Good Cooking',
+  'A visionary new master class in cooking that distills decades of professional experience into just four simple elements, from the woman declared “America’s next great cooking teacher” by Alice Waters.In the tradition of The Joy of Cooking and How to Cook Everything comes Salt, Fat, Acid, Heat, an ambitious new approach to cooking by a major new culinary voice. Chef and writer Samin Nosrat has taught everyone from professional chefs to middle school kids to author Michael Pollan to cook using her revolutionary, yet simple, philosophy. Master the use of just four elements—Salt, which enhances flavor; Fat, which delivers flavor and generates texture; Acid, which balances flavor; and Heat, which ultimately determines the texture of food—and anything you cook will be delicious. By explaining the hows and whys of good cooking, Salt, Fat, Acid, Heat will teach and inspire a new generation of cooks how to confidently make better decisions in the kitchen and cook delicious meals with any ingredients, anywhere, at any time.Echoing Samin’s own journey from culinary novice to award-winning chef, Salt, Fat Acid, Heat immediately bridges the gap between home and professional kitchens. With charming narrative, illustrated walkthroughs, and a lighthearted approach to kitchen science, Samin demystifies the four elements of good cooking for everyone. Refer to the canon of 100 essential recipes—and dozens of variations—to put the lessons into practice and make bright, balanced vinaigrettes, perfectly caramelized roast vegetables, tender braised meats, and light, flaky pastry doughs.Featuring 150 illustrations and infographics that reveal an atlas to the world of flavor by renowned illustrator Wendy MacNaughton, Salt, Fat, Acid, Heat will be your compass in the kitchen. Destined to be a classic, it just might be the last cookbook you’ll ever need.',
+  'https://images-na.ssl-images-amazon.com/images/I/81ABBZDod%2BL._AC_SR201,266_.jpg',
+  '2018-04-25');
+
+INSERT INTO book_details (id, isbn, title, description, cover_picture_url, publication_date) VALUES (
+  6,
+  '9780553380163',
+  'A Brief History of Time',
+  'A landmark volume in science writing by one of the great minds of our time, Stephen Hawking’s book explores such profound questions as: How did the universe begin—and what made its start possible? Does time always flow forward? Is the universe unending—or are there boundaries? Are there other dimensions in space? What will happen when it all ends?
+Told in language we all can understand, A Brief History of Time plunges into the exotic realms of black holes and quarks, of antimatter and “arrows of time,” of the big bang and a bigger God—where the possibilities are wondrous and unexpected. With exciting images and profound imagination, Stephen Hawking brings us closer to the ultimate secrets at the very heart of creation.',
+  'https://images-na.ssl-images-amazon.com/images/I/51%2BGySc8ExL._SX333_BO1,204,203,200_.jpg',
+  '1998-09-01');
+INSERT INTO book_details (id, isbn, title, description, cover_picture_url, publication_date) VALUES (
+  7,
+  '9780451524935',
+  '1984 (Signet Classics)',
+  '“The Party told you to reject the evidence of your eyes and ears. It was their final, most essential command.”Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching...A startling and haunting vision of the world, 1984 is so powerful that it is completely convincing from start to finish. No one can deny the influence of this novel, its hold on the imaginations of multiple generations of readers, or the resiliency of its admonitions—a legacy that seems only to grow with the passage of time.',
+  'https://images-na.ssl-images-amazon.com/images/I/31lWUHDG7uL._SX282_BO1,204,203,200_.jpg',
+  '1950-07-01');
+
+
+INSERT INTO book_details_authors VALUES (1, 7);
+INSERT INTO book_details_authors VALUES (2, 8);
+INSERT INTO book_details_authors VALUES (3, 10);
+INSERT INTO book_details_authors VALUES (4, 11);
+INSERT INTO book_details_authors VALUES (5, 12);
+INSERT INTO book_details_authors VALUES (6, 13);
+INSERT INTO book_details_authors VALUES (7, 14);
+
+INSERT INTO book_details_categories VALUES (1, 3);
 INSERT INTO book_details_categories VALUES (2, 3);
-INSERT INTO book_details_categories VALUES (3, 2);
 INSERT INTO book_details_categories VALUES (3, 1);
-INSERT INTO book_details_categories VALUES (4, 2);
-INSERT INTO book_details_categories VALUES (4, 1);
-INSERT INTO book_details_categories VALUES (5, 2);
-INSERT INTO book_details_categories VALUES (5, 1);
-INSERT INTO book_details_categories VALUES (8, 2);
-INSERT INTO book_details_categories VALUES (8, 1);
-
-INSERT INTO author (author_full_name) VALUES ('Edgar Jander');
-INSERT INTO author (author_full_name) VALUES ('Lucas Newell');
-INSERT INTO author (author_full_name) VALUES ('Robbie Torok');
-INSERT INTO author (author_full_name) VALUES ('Raymond Kertis');
-INSERT INTO author (author_full_name) VALUES ('Emmet Brenig');
-INSERT INTO author (author_full_name) VALUES ('Andrzej Sapkowski');
-
-INSERT INTO book_details_authors VALUES (1, 1);
-INSERT INTO book_details_authors VALUES (2, 1);
-INSERT INTO book_details_authors VALUES (3, 1);
-INSERT INTO book_details_authors VALUES (4, 2);
-INSERT INTO book_details_authors VALUES (5, 2);
-INSERT INTO book_details_authors VALUES (6, 3);
-INSERT INTO book_details_authors VALUES (7, 4);
-INSERT INTO book_details_authors VALUES (7, 5);
+INSERT INTO book_details_categories VALUES (4, 5);
+INSERT INTO book_details_categories VALUES (5, 4);
+INSERT INTO book_details_categories VALUES (6, 5);
+INSERT INTO book_details_categories VALUES (6, 7);
+INSERT INTO book_details_categories VALUES (7, 7);
 
 
-INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('23243', 1, 1, 'no disc');
-INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('43544', 1, 2, 'ok');
-INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('32434', 2, 2, 'missing pages');
-INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('23453', 3, 3, 'ok');
-INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('12312', 4, 3, 'missing pages');
-INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('12311', 5, 1, 'ok');
-INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('23451', 6, 1, 'ok');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('booka', 1, 1, 'dirty');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('bookaAA', 1, 2, 'putti');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('bookB', 2, 2, 'missing pages');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('YHNANK', 3, 3, 'bad');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('ALMNUM', 4, 3, 'cool');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('SIG123', 5, 1, 'stupid');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('OUF', 6, 1, 'chairman');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('ui ui', 7, 1, 'gut');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('miu miau', 7, 2, 'missing cover');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('nubu', 7, 3, 'juj');
+INSERT INTO book (signature, book_details_id, book_status_id, comments) VALUES ('pook', 7, 3, 'is fine');
