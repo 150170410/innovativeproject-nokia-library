a8100235c82a82d04ba40f85e2ee81ef0c27739e
tubidubidam <jacekzalewski95@gmail.com>
2018-12-22 12:53:32 +0100

    rest service headers

--- frontend/src/app/services/rest/rest.service.ts
+++ frontend/src/app/services/rest/rest.service.ts
@@ -10,36 +10,42 @@ import { MessageInfo } from '../../models/MessageInfo';
 })
 export class RestService {
 	URL = API_URL + '/api/v1/';
-	httpOptions = {
-		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-	};
 
 	constructor(private http: HttpClient) {
 	}
 
+	setHeaders() {
+		return {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Authorization': sessionStorage.getItem('token')
+			})
+		};
+	}
+
 	async getAll(url: string) {
-		return await this.http.get<MessageInfo>(this.URL + url)
+		return await this.http.get<MessageInfo>(this.URL + url, this.setHeaders())
 		.pipe(
 			catchError(this.handleError)
 		).toPromise();
 	}
 
 	save(url: string, item: any): Observable<any> {
-		return this.http.post<any>(this.URL  + url + '/create', item, this.httpOptions)
+		return this.http.post<any>(this.URL + url + '/create', item, this.setHeaders())
 		.pipe(
 			catchError(this.handleError)
 		);
 	}
 
 	update(url: string, id: number, item: any) {
-		return this.http.post<any>(this.URL + url + '/update/' + `${id}`, item, this.httpOptions)
+		return this.http.post<any>(this.URL + url + '/update/' + `${id}`, item, this.setHeaders())
 		.pipe(
 			catchError(this.handleError)
 		);
 	}
 
 	remove(url: string, id: number) {
-		return this.http.delete<any>(this.URL + url + '/remove/' + `${id}`)
+		return this.http.delete<any>(this.URL + url + '/remove/' + `${id}`, this.setHeaders())
 		.pipe(
 			catchError(this.handleError)
 		);
