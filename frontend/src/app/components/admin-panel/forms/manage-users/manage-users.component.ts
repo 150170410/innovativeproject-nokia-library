5192b6ac5a7a0a5a16aab71b2249d5ec2f7fb826
tubidubidam <jacekzalewski95@gmail.com>
2019-01-05 23:39:34 +0100

    frontend side of manage-users section in admin panel

--- frontend/src/app/components/admin-panel/forms/manage-users/manage-users.component.ts
+++ frontend/src/app/components/admin-panel/forms/manage-users/manage-users.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { User } from '../../../../models/database/entites/User';
 
 @Component({
-  selector: 'app-manage-users',
-  templateUrl: './manage-users.component.html',
-  styleUrls: ['./manage-users.component.css']
+	selector: 'app-manage-users',
+	templateUrl: './manage-users.component.html',
+	styleUrls: ['./manage-users.component.css', '../../admin-panel.component.css']
 })
 export class ManageUsersComponent implements OnInit {
 
-  constructor() { }
+	// table
+	@ViewChild('paginator') paginator: MatPaginator;
+	dataSource = new MatTableDataSource<User>();
+	displayedColumns: string[] = ['id', 'email', 'fullName', 'role', 'address', 'actions'];
 
-  ngOnInit() {
-  }
+	constructor() {
+	}
 
+	ngOnInit() {
+		this.getUsers();
+	}
+
+	getUsers() {
+		// const response: MessageInfo = await this.http.getAll('books/getAll');
+		this.dataSource = new MatTableDataSource(User.all());
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.filterPredicate = (data, filter: string) => {
+			return JSON.stringify(data).toLowerCase().includes(filter.toLowerCase());
+		};
+	}
+
+	promoteUser(user: User) {
+
+	}
+
+	demoteUser(user: User) {
+
+	}
 }
--- frontend/src/app/models/database/entites/User.ts
+++ frontend/src/app/models/database/entites/User.ts
@@ -8,4 +8,22 @@ export class User {
 	address: any;
 	roles: any;
 	books: Book[];
+
+	constructor(id: number, email: string, firstName: string, lastName: string, address: any, roles: any, books: Book[]) {
+		this.id = id;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.roles = roles;
+		this.books = books;
+	}
+
+	 static all(){
+		return [
+			new User(1, 'janusz.pawlacz@nokia.com', 'Janusz', 'Pawlacz', 'west link', 'user', []),
+			new User(2, 'jacek.zalewski@email.com', 'Jacek', 'Zalewski', 'nowhere', 'admin', [])
+		];
+	}
+
 }
