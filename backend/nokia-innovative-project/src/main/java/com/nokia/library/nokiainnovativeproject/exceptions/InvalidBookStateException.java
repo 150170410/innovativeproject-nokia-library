4b70f7df93638ec07ccedaeaee939c9c8461b925
Mikolaj <mikolaj.banaszkiewicz2@gmail.com>
2019-01-08 23:38:46 +0100

    exception cleanup, fixes

---
+++ backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/exceptions/InvalidBookStateException.java
@@ -0,0 +1,4 @@
+package com.nokia.library.nokiainnovativeproject.exceptions;
+
+public class InvalidBookStateException {
+}
---
+++ backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/utils/ReservationByDateComparator.java
@@ -0,0 +1,4 @@
+package com.nokia.library.nokiainnovativeproject.utils;
+
+public class ReservationDateComparator {
+}
