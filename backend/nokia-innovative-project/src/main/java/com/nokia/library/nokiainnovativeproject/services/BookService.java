cff10c5447e52e99522ed1e67dfbc7b1be1de269
SpokoSpartan <wojtekspotn@gmail.com>
2019-01-18 21:24:38 +0100

    problem with actualOwners solved and email added

--- backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/services/BookService.java
+++ backend/nokia-innovative-project/src/main/java/com/nokia/library/nokiainnovativeproject/services/BookService.java
@@ -27,7 +27,7 @@ public class BookService {
 	private final BookDetailsRepository bookDetailsRepository;
 	private final BookStatusRepository bookStatusRepository;
 	private final BookStatusService bookStatusService;
-	private final UserRepository userRepository;
+	private final UserService userService;
 
 
 	public List<Book> getAllBooks() {
@@ -54,6 +54,7 @@ public class BookService {
 		ModelMapper mapper = new ModelMapper();
 		Book book = mapper.map(bookDTO, Book.class);
 		book.setAvailableDate(LocalDateTime.now());
+		book.setActualOwnerId(userService.getLoggedInUser().getId());
 		return bookRepository.save(persistRequiredEntities(book, bookDTO));
 	}
 
@@ -62,6 +63,7 @@ public class BookService {
 		book.setComments(bookDTO.getComments());
 		book.setSignature(bookDTO.getSignature());
 		book.getBookDetails().setIsRemovable(true);
+		book.setActualOwnerId(userService.getLoggedInUser().getId());
 		return bookRepository.save(persistRequiredEntities(book, bookDTO));
 	}
 
@@ -90,6 +92,7 @@ public class BookService {
 	public Book changeState(Book book, Long newStatusId, Integer days, User newOwner) {
 		BookStatus newStatus = bookStatusService.getBookStatusById(newStatusId);
 		book.setStatus(newStatus);
+		book.setActualOwnerId(newOwner.getId());
 		LocalDateTime oldAvailableDate = book.getAvailableDate();
 		if(oldAvailableDate == null){
 			oldAvailableDate = LocalDateTime.now();
